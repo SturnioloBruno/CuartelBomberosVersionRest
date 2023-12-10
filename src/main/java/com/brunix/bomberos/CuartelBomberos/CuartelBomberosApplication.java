7aa497bbc88a859740be41731f379bf8eb5ae30e
sturniolo <br1.sturniolo@gmail.com>
2023-12-10 08:48:54 -0300

    [firstCommit]genesis

---
+++ src/main/java/com/brunix/bomberos/CuartelBomberos/CuartelBomberosApplication.java
@@ -0,0 +1,13 @@
+package com.brunix.bomberos.CuartelBomberos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuartelBomberosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuartelBomberosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunix/bomberos/CuartelBomberos/CuartelBomberosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunix.bomberos.CuartelBomberos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuartelBomberosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
